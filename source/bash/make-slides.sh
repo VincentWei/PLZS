8a4237ee36eaca9693c5947ee85178211be58f23
Vincent Wei <vincent@minigui.org>
2024-05-22 17:06:20 +0800

    rearrange math course

--- source/bash/make-slides.sh
+++ source/bash/make-slides.sh
@@ -6,8 +6,9 @@ IFS=':'
 while read -ra LINE; do
     html_file="${LINE[0]}.html"
     if [ -f $html_file ]
-        echo "Skip $html_file"
     then
+        echo "Skip $html_file"
+    else
         cp template.html $html_file
         sed -i '' -e "s/TITLE/${LINE[1]}/g" $html_file
         sed -i '' -e "s/FILENAME/${LINE[0]}/g" $html_file
@@ -15,8 +16,9 @@ while read -ra LINE; do
 
     md_file="${LINE[0]}.md"
     if [ -f $md_file ]
-        echo "Skip $md_file"
     then
+        echo "Skip $md_file"
+    else
         echo "# ${LINE[1]}" > $md_file
     fi
 done < "$file"
