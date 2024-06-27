69a9a2df08f11543688053be6863a5e40dd735de
Vincent Wei <vincent@minigui.org>
2024-06-27 09:06:25 +0800

    initial version

---
+++ source/cpp/lesson-1/numeric-types.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "The size of char is " << sizeof(char) << std::endl;
+    std::cout << "The size of short is " << sizeof(short) << std::endl;
+    std::cout << "The size of int is " << sizeof(int) << std::endl;
+    std::cout << "The size of long is " << sizeof(long) << std::endl;
+    std::cout << "The size of long long is " << sizeof(long long) << std::endl;
+    std::cout << "The size of float is " << sizeof(float) << std::endl;
+    std::cout << "The size of double is " << sizeof(double) << std::endl;
+    std::cout << "The size of long double is " << sizeof(long double) << std::endl;
+}
