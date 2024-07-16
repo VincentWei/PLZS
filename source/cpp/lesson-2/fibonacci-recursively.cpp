efde4db6b1773e69e9fca81a3e1a0b23269ec9c3
VincentWei <vincent@minigui.org>
2024-07-16 14:25:58 +0800

    add some comments

--- source/cpp/lesson-2/fibonacci-recursively.cpp
+++ source/cpp/lesson-2/fibonacci-recursively.cpp
@@ -3,30 +3,31 @@
 
 using namespace std;
 
+// returns the nth Fibonacci number
 unsigned fibonacci(unsigned n)
 {
     if (n < 2)
         return 1;
-
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int main()
 {
     unsigned max;
-    cin >> max;
+    cin >> max;                     // Gets the maximal integer from user.
 
-    cout << "1, 1";
-    unsigned n = 2;
+    cout << "1, 1";                 // Prints the first two Fibonacci numbers.
+    unsigned n = 3;                 // Starts from the 3rd Fibonacci numbers.
     while (true) {
-        unsigned c = fibonacci(n);
-        if (c >= max)
-            break;
+        unsigned c = fibonacci(n);  // Gets the nth Fibonacci number.
+        if (c >= max)               // Breaks the loop if the number
+            break;                  //      larger than the maximum.
 
-        cout << ", " << c;
-        n++;
+        cout << ", " << c;          // Prints the Fibonacci number.
+        n++;                        // Forwards to the next Fibonacci number.
     }
 
+    // Prints the dot and the end of the line character.
     cout << "." << endl;
 }
 
