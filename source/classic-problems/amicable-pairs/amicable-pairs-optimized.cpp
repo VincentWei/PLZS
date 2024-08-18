d366cba89530a648b43f69b4d43aff90d3991efd
Vincent Wei <vincent@minigui.org>
2024-08-18 18:02:08 +0800

    tune

--- source/classic-problems/amicable-pairs/amicable-pairs-optimized.cpp
+++ source/classic-problems/amicable-pairs/amicable-pairs-optimized.cpp
@@ -16,18 +16,17 @@
 #include <cassert>      // for assert()
 
 using namespace std;
-using uint64_v = vector<uint64_t>;
+using intmax_v = vector<intmax_t>;
 
-uint64_t summary_of_true_factors(uint64_t n)
+intmax_t summary_of_true_factors(intmax_t n)
 {
-    uint64_t summary = 1;
-    uint64_t max = llroundl(sqrtl(n));
-    for (uint64_t i = 2; i <= max; i++) {
+    intmax_t summary = 1;
+    for (intmax_t i = 2; i * i <= n; i++) {
         imaxdiv_t div = imaxdiv(n, i);
 
         if (div.rem == 0) {
             summary += i;
-            if (i != max)
+            if (i != n)
                 summary += div.quot;
         }
     }
@@ -40,16 +39,18 @@ int main()
     assert(summary_of_true_factors(220) == 284);
     assert(summary_of_true_factors(284) == 220);
 
-    uint64_v found;
-    uint64_t n = 200;
-    while (true) {
+    intmax_v found;
+    intmax_t n = 200;
+    unsigned nr = 0;
+    while (n < INTMAX_MAX) {
         if (!binary_search(found.begin(), found.end(), n)) {
-            uint64_t other = summary_of_true_factors(n);
+            intmax_t other = summary_of_true_factors(n);
             if (other > n && summary_of_true_factors(other) == n) {
-                cout << n << " " << other << endl;
+                cout << nr << ":\t" << n << " " << other << endl;
                 found.push_back(n);
                 found.push_back(other);
                 sort(found.begin(), found.end());
+                nr++;
             }
         }
 
