0339ce68df08e105f44ac1bdb06c606d1d2c5e61
Vincent Wei <vincent@minigui.org>
2024-10-15 10:04:28 +0800

    update

--- source/classic-problems/bezout/bezout.cpp
+++ source/classic-problems/bezout/bezout.cpp
@@ -18,18 +18,7 @@
 
 using namespace std;
 
-int gcd_l(int a, int b)
-{
-    while (b != 0) {
-        int tmp = a;
-        a = b;
-        b = tmp % b;
-    }
-
-    return a;
-}
-
-int ex_gcd(int a, int b, int& x, int& y)
+intmax_t ex_gcd_r(intmax_t a, intmax_t b, intmax_t& x, intmax_t& y)
 {
     if (b == 0) {
         x = 1;
@@ -37,16 +26,35 @@ int ex_gcd(int a, int b, int& x, int& y)
         return a;
     }
 
-    int gcd = ex_gcd(b, a % b, x, y);
-    int temp = x;
+    intmax_t gcd = ex_gcd_r(b, a % b, x, y);
+    intmax_t temp = x;
     x = y;
     y = temp - a / b * y;
     return gcd;
 }
 
+intmax_t ex_gcd_i(intmax_t a, intmax_t b, intmax_t& x, intmax_t& y)
+{
+    intmax_t prev_x, prev_y;
+
+    prev_x = 0; prev_y = 1;
+    x = 1; y = 0;
+    while (b != 0) {
+        intmax_t q = a / b;
+
+        intmax_t tmp;
+        tmp = prev_x; prev_x = prev_x - q * x; prev_x = x;
+        tmp = prev_y; prev_y = prev_y - q * y; prev_y = y;
+        tmp = b; b = a % b; a = tmp;
+    }
+
+    x = prev_x; y = prev_y;
+    return a;
+}
+
 int main()
 {
-    int a, b, d;
+    intmax_t a, b, d;
     cin >> a >> b >> d;
 
     if (a == 0 || b == 0 || d == 0) {
@@ -54,14 +62,14 @@ int main()
         return 0;
     }
 
-    int x0, y0;
-    int gcd = ex_gcd(a, b, x0, y0);
+    intmax_t x0, y0;
+    intmax_t gcd = ex_gcd_r(a, b, x0, y0);
     if (d % gcd != 0) {
         cout << "NO SOLUTION" << endl;
         return 0;
     }
 
-    int m0 = d / gcd;
+    intmax_t m0 = d / gcd;
     x0 *= m0;
     y0 *= m0;
 
@@ -69,8 +77,8 @@ int main()
     assert(a * x0 + b * y0 == d);
 
     for (int k = 1; k < 10; k++) {
-        int x = x0 + k * b / gcd;
-        int y = y0 - k * a / gcd;
+        intmax_t x = x0 + k * b / gcd;
+        intmax_t y = y0 - k * a / gcd;
         cout << x << " " << y << endl;
         assert(a * x + b * y == d);
     }
